82f2a8f0ebd4cd7cd6f88a85b689532cbb82e436
Santi Depetris <depetris.santi@gmail.com>
2020-02-21 12:55:48 -0300

    Transfer the actions in the Parks controller at Cars controller

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,22 +1,2 @@
 class ParksController < ApplicationController
-	def index
-	end
-
-	def create
-	end
-
-	def new
-	end
-
-	def edit
-	end
-
-	def show
-	end
-
-	def update
-	end
-
-	def destroy
-	end
 end
