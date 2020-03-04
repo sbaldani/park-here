cdeec51c9e34cad816e6d581f01618299c2ddeea
Santi Depetris <depetris.santi@gmail.com>
2020-03-04 10:37:20 -0300

    Add models for Active Admin

---
+++ app/admin/cars.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Car do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :registration_plate, :type_of_car, :model, :year, :color, :brand, :user_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:registration_plate, :type_of_car, :model, :year, :color, :brand, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/rents.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Rent do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :owner_id, :client_id, :car_id, :park_id, :price, :date_from, :date_to
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:owner_id, :client_id, :car_id, :park_id, :price, :date_from, :date_to]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
