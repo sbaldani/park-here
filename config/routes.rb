5a2c7fb6c1a21f13d2c9787cb05fe1724f242729
Santi Depetris <depetris.santi@gmail.com>
2020-03-13 16:19:45 -0300

    Create controler for dashboards and configurate routes for dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   ActiveAdmin.routes(self)
   get 'home/index'
+  get 'dashboards/index'
   resources :users
   resources :cars
   resources :parks
